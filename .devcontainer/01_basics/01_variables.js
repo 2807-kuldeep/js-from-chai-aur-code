bca5cf2b3673be118a3f0852fd21cee959afdc40
kuldeep prajapati <kuldeepp28112007@gmail.com>
2026-02-20 14:59:30 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=1243
+let accountmail="kuldeep2811@gmail.com"
+var accountpassword="12345"
+accountcity="kanpur"
+// accountId=2 not allowed
+
+accountmail = "some@12.com"
+accountpassword = "23567"
+accountcity = "lal"
+
+
+console.log(accountId);
+console.table([accountId,accountmail,accountpassword,accountcity])
